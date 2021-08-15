c2e2c7512c03112f3108120343e21b13744a49ba
geoffrey45 <geoffreymungai45@gmail.com>
2021-08-15 22:07:39 +0300

    one very big buggy commit

--- frontend/script.js
+++ frontend/script.js
@@ -27,7 +27,7 @@ function printFolders(){
             folderNode.setAttribute('id', 'folder');
 
             folderNode.innerHTML = folder;
-            console.log(folder);
+            // console.log(folder);
 
             appendNode(container, folderNode);
         });
@@ -40,7 +40,7 @@ function getFiles(folder){
     return fetch('http://localhost:8080/' + folder)
     .then(response => response.json())
     .then(json => {
-        console.log(json);
+        // console.log(json);
         return json.all_files;
     });
 }
@@ -76,54 +76,152 @@ function updateFiles(){
 
 }
 
-updateFiles();
+// updateFiles();
 
-function loadAudio(file){
-    let home_folder;
-    let folder_name;
-    let file_name;
+// function loadAudio(file){
+//     let home_folder;
+//     let folder_name;
+//     let file_name;
 
-    let folders = getContainer('folders');
-    let files = getContainer('files');
 
-    const getFolderName = async() => {
-        folders.addEventListener('click', function(event){
-            if (event.target.id === 'folder') {
-                folder_name = event.target.innerHTML;
 
-                const file_name = await getFileName();
-                getHomeFolder();
+    // let folders = getContainer('folders');
+    // let files = getContainer('files');
 
-                console.log(home_folder + '/' + folder_name + '/' + file_name);
-            };
-        });
-    };
-
-    function getFileName(){
-        files.addEventListener('click', function(event){
-
-            if (event.target.id === 'file') {
-                return event.target.innerHTML;
-            };
-        });
-
-    };
     
-    function getHomeFolder(){
-        return fetch('http://localhost:8080/')
-            .then(response => response.json())
-            .then(json => {
-                home_folder = json.home_folder;
-                console.log(home_folder);
-            }
-            // _callback();
-            );
+    // async function getFolderName(){
+    //     return hello = await Promise.resolve(sayHi())
+    // };
 
-        
-    };
+    // getFolderName().then(alert)
 
-    return getFolderName();
-};
 
-let audio = loadAudio();
-console.log(audio);
+    // getFolderName()
+    // function getFileName(){
+    //     files.addEventListener('click', function(event){
+
+    //         if (event.target.id === 'file') {
+    //             return event.target.innerHTML;
+    //         };
+    //     });
+
+    // };
+    
+    // function getHomeFolder(){
+    //     return fetch('http://localhost:8080/')
+    //         .then(response => response.json())
+    //         .then(json => {
+    //             home_folder = json.home_folder;
+    //             console.log(home_folder);
+    //         }
+    //         );
+    //     }
+// };
+
+// loadAudio()
+
+
+// function g(){
+//     console.log('ggggggggg')
+// }
+
+// function h(){
+//     return g
+// }
+
+// console.log(h())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let folder = document.getElementById('folders');
+let f = document.getElementById('container')
+
+async function one(){
+    return new Promise((resolve, reject) => {
+        folder.addEventListener('click', resolve)
+        f.addEventListener('click', reject)
+    }) 
+}
+
+async function waitClick(){
+    await one()
+    .then((e) => {
+        if (e.target.id === 'folder') {
+            return (e.target.innerHTML)
+        }
+    })
+    .then((value) => {
+        console.log(value)
+        printFiles(value)
+    })
+}
+
+waitClick()
+// function a(){
+//     console.log('a')
+// }
+
+// async function waitClick(){
+//     await one()
+//     .then((e) => {
+//         a()
+//         console.log(e)
+//     })
+//     .catch(() => {a()})
+// }
+
+// waitClick()
+// async function getFolder(){
+//     folders.addEventListener('click', function(event){
+//         if (event.target.id === 'folder') {
+//             folder_name = event.target.innerHTML;
+
+//             console.log(folder_name)
+//             return await folder_name
+//         };
+//     })
+// }
+
+// getFolder().then(console.log)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
